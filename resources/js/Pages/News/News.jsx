88932ce4a07c8b88968b2576ecaa11a22d03544c
Vinnie <34916@ma-web.nl>
2025-01-16 17:13:46 +0100

    styling the news page a bit

--- resources/js/Pages/News/News.jsx
+++ resources/js/Pages/News/News.jsx
@@ -1,8 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { usePage } from "@inertiajs/react";
+import { Head, usePage } from "@inertiajs/react";
 
 function News(){
-    console.log(usePage().props);
     const news = usePage().props.news;
     const users = usePage().props.users;
     const dataSets = news?.map(item => 
@@ -11,18 +10,22 @@ function News(){
                 <img src={item.image ? item.image : `https://rezista.in/wp-content/uploads/2020/07/Image-Placeholder-Dark.png`} alt={item.image ? item.title : 'placeholder image'} className="news__item--figure-image" />
             </figure>
             <div className="news__item--wrapper">
-                <h2 className="news__item--title">{item.title}</h2>
-                <p className="news__item--description">{item.description}</p>
+                <article className="news__item--info">
+                    <h2 className="news__item--info-title">{item.title}</h2>
+                    <p className="news__item--info-description">{item.description}</p>
+                </article>
                 <span className="news__item--details">
                     <p className="news__item--details-author">{users?.find((id) => users.id === news.user_id).name}</p>
-                    <p className="news__item--details-date">{new Date(item.created_at).toLocaleDateString('en-UK', {day: 'numeric', month: 'long', year: 'numeric'})}</p>
+                    <p className="news__item--details-date">{new Date(item.created_at).toLocaleDateString('en-UK', {day: 'numeric', month: 'short', year: 'numeric'})}</p>
                 </span>
             </div>
         </article>
     );
     return(
         <AuthenticatedLayout>
+            <Head title="News" />
             <section className="news">
+                <p className="news__amount">{news?.length} NIEUWS BERICHTEN</p>
                 <div className="news__list">
                     {dataSets}
                 </div>
